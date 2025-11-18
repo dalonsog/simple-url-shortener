04db43fb58682b633d0b3f8c61e2c1a0ad24f04f
Daniel <daniel@Daniels-MacBook-Pro.local>
2025-11-18 10:53:35 +0100

    Added test dependency to github actions workflow

--- urlshortener/api/__init__.py
+++ urlshortener/api/__init__.py
@@ -6,10 +6,10 @@ from urlshortener.api.config import Settings
 def register_routes(app: Flask) -> None:
     from urlshortener.api.routes.auth import bp as auth_bp
     app.register_blueprint(auth_bp)
-    
+
     from urlshortener.api.routes.user import bp as user_bp
     app.register_blueprint(user_bp, url_prefix='/users')
-    
+
     from urlshortener.api.routes.url import bp as url_bp
     app.register_blueprint(url_bp)
 
@@ -32,9 +32,9 @@ def create_app() -> Flask:
         "port": Settings.REDIS_PORT,
         "password": Settings.REDIS_PASSWORD
     }
-    
+
     db.init_app(app)
 
-    register_routes(app)    
+    register_routes(app)
 
     return app
